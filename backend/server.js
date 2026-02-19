f320a1a1d66f548703d6d433996cf7f3aae13078
vidhya <anasanividhyadhari77@gmail.com>
2026-02-19 15:24:48 +0530

    made UI changes and added logout page

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:8080', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5134', 'http://localhost:8080', 'http://localhost:3000'],
     credentials: true
 };
 
